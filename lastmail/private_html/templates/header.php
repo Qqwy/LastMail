1708fe46ec7c2bc2496dc8a75b73c4368c7c82f3
Qqwy <qqwy@gmx.com>
2014-07-09 22:43:47 +0200

    Social Media integration

--- lastmail/private_html/templates/header.php
+++ lastmail/private_html/templates/header.php
@@ -92,10 +92,18 @@
 		<meta property="og:title" content="Last Mail | The world's first passive post-mortem message system." />
 		<meta property="og:url" content="https://last-mail.org" />
 		<meta property="og:locale" content="en_US" />
+		
+		<meta property="og:site_name" content="LastMail" />
 
 		<meta property="og:description" content="LastMail lets you send a good-bye to your friends and contacts, and pass on your (digital) belongings after you pass on." />
 		<meta name="description" content="LastMail lets you send a good-bye to your friends and contacts, and pass on your (digital) belongings after you pass on." />
-
+		
+	<meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@W_MCode">
+    <meta name="twitter:creator" content="@W_MCode">
+    <meta name="twitter:title" content="Last Mail | The world's first passive post-mortem message system.">
+    <meta name="twitter:description" content="LastMail lets you send a good-bye to your friends and contacts, and pass on your (digital) belongings after you pass on.">
+    <meta name="twitter:image:src" content="http://last-mail.org/lastmailscreen.jpg">
 
 	</head>
 	<?php if(!isset($indexpage)){ ?>
